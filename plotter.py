bb5d7eaaf6dfaf804b353d8f645a556b9d3ab29c
auhtwoo <32828120+auhtwoo@users.noreply.github.com>
2017-10-15 23:46:53 -0400

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -19,7 +19,7 @@ class Plotter:
         self.frame_listener = frame_listener
         fig = plt.figure()
         ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
-        self.lines = [ax.plot([], [], lw=2)[0] for j in range(4)]
+        self.lines = [ax.plot([], [], lw=2)[0] for j in range(5)]
 
         # For some reason we must store the return value somewhere, otherwise it doesn't work!
         _ = animation.FuncAnimation(fig, animate, init_func=plot_init, frames=200, interval=20, blit=True)
